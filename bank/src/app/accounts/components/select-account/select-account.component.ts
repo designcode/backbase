c92c0fc1f0460e43653925c2bbd7071c24ca384a
A Ibrahim <abdullah.ibrahim@asellion.com>
2020-11-25 18:42:31 +0100

    Some styling

--- bank/src/app/accounts/components/select-account/select-account.component.ts
+++ bank/src/app/accounts/components/select-account/select-account.component.ts
@@ -1,15 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'bank-select-account',
+  selector: 'app-bank-select-account',
   templateUrl: './select-account.component.html',
-  styleUrls: ['./select-account.component.scss']
+  styleUrls: ['./select-account.component.scss'],
 })
 export class SelectAccountComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
